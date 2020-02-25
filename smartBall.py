24798d7b334e46657ff51f2fdd031dab4eabe528
talhakhan1297 <talhakhan1297@yahoo.com>
2020-02-25 15:13:03 +0500

    Added real time plot

--- smartBall.py
+++ smartBall.py
@@ -4,6 +4,7 @@ import pyaudio
 import wave
 import threading
 import matplotlib
+import numpy as np
 import matplotlib.animation as animation
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import *
@@ -14,26 +15,18 @@ style.use('ggplot')
 
 f = Figure(figsize=(9, 3), dpi=100)
 a = f.add_subplot(111)
+intframes = []
 
 
 def animate(i):
-    pullData = open("sampleText.txt", "r").read()
-    dataList = pullData.split('\n')
-    xList = []
-    yList = []
-    for eachLine in dataList:
-        if len(eachLine) > 1:
-            x, y = eachLine.split(',')
-            xList.append(int(x))
-            yList.append(int(y))
-
     a.clear()
-    a.plot(xList, yList)
+    a.plot(intframes)
 
 
 class App:
 
     def __init__(self, master):
+
         # Colors defination
         backgroundColor = '#2C2C2C'
         primaryColor = '#FFFFFF'
@@ -47,14 +40,14 @@ class App:
         self.sample_format = pyaudio.paInt16  # 16 bits per sample
 
         # Initialized dynamic variables
-        self.radioVal = IntVar()
-        self.recordingState = False
+        self.radioVal = StringVar()
         self.count = 0
         self.frames = []  # Initialize array to store frames
         self.isrecording = False
         self.p = pyaudio.PyAudio()  # Create an interface to PortAudio
         self.stream = self.p.open(format=self.sample_format, channels=self.channels,
                                   rate=self.fs, frames_per_buffer=self.chunk, input=True)
+        self.fileName = ''
 
         # Initialized Main Window
         master.title("SmartBall Sampling App")
@@ -64,7 +57,6 @@ class App:
 
         # Initialized Plot
         plotCanvas = FigureCanvasTkAgg(f, master)
-        # canvas.show()
         plotCanvas.get_tk_widget().grid(row=0, padx=(45, 45), pady=(24, 24), columnspan=5)
 
         # Initialized Record Button
@@ -87,57 +79,66 @@ class App:
         # Initialized Radio Buttons
         r1 = Radiobutton(master, text="Edge", bg=backgroundColor, font=("Montserrat 12"), activebackground=backgroundColor,
                          fg=primaryColor, highlightcolor=backgroundColor, borderwidth=0, selectcolor=backgroundColor,
-                         variable=self.radioVal, value=1, command=self.radioSelect)
+                         variable=self.radioVal, value="edge", command=self.radioSelect)
         r1.grid(row=3)
 
         r2 = Radiobutton(master, text="NC",
                          bg=backgroundColor, font=("Montserrat 12"), activebackground=backgroundColor, fg=primaryColor,
                          highlightcolor=backgroundColor, borderwidth=0, selectcolor=backgroundColor, variable=self.radioVal,
-                         value=2, command=self.radioSelect)
+                         value="nc", command=self.radioSelect)
         r2.grid(row=3, column=1)
 
         r3 = Radiobutton(master, text="Pad",
                          bg=backgroundColor, font=("Montserrat 12"), activebackground=backgroundColor, fg=primaryColor,
-                         highlightcolor=backgroundColor, borderwidth=0, selectcolor=backgroundColor, variable=self.radioVal, value=3,
-                         command=self.radioSelect)
+                         highlightcolor=backgroundColor, borderwidth=0, selectcolor=backgroundColor, variable=self.radioVal,
+                         value="pad", command=self.radioSelect)
         r3.grid(row=3, column=2)
 
-        r4 = Radiobutton(master, text="Pad",
+        r4 = Radiobutton(master, text="Middle",
                          bg=backgroundColor, font=("Montserrat 12"), activebackground=backgroundColor, fg=primaryColor,
-                         highlightcolor=backgroundColor, borderwidth=0, selectcolor=backgroundColor, variable=self.radioVal, value=4,
-                         command=self.radioSelect)
+                         highlightcolor=backgroundColor, borderwidth=0, selectcolor=backgroundColor, variable=self.radioVal,
+                         value="middle", command=self.radioSelect)
         r4.grid(row=3, column=3)
 
-        r5 = Radiobutton(master, text="Pad",
+        r5 = Radiobutton(master, text="Other",
                          bg=backgroundColor, font=("Montserrat 12"), activebackground=backgroundColor, fg=primaryColor,
-                         highlightcolor=backgroundColor, borderwidth=0, selectcolor=backgroundColor, variable=self.radioVal, value=5,
-                         command=self.radioSelect)
+                         highlightcolor=backgroundColor, borderwidth=0, selectcolor=backgroundColor, variable=self.radioVal,
+                         value="other", command=self.radioSelect)
         r5.grid(row=3, column=4)
 
         # Initialized Save Button
         saveBtn = Button(master, text='Save', font=(
-            "Montserrat 10 bold"), bg=accentColor, activebackground=pressedButton, fg=primaryColor, bd=0, pady=5, padx=15)
+            "Montserrat 10 bold"), bg=accentColor, activebackground=pressedButton, fg=primaryColor, bd=0, pady=5, padx=15,
+            command=self.save)
         saveBtn.grid(row=4, column=2, pady=(24, 5), padx=15)
 
     # Handle radio selection
     def radioSelect(self):
-        selection = str(self.radioVal.get())
+        if self.frames.__len__() != 0 and self.isrecording == False:
+            self.fileName = str(self.count) + '_' + \
+                self.radioVal.get() + '.wav'
 
     # Start Recording
     def startRecording(self):
-        self.p = pyaudio.PyAudio()  # Create an interface to PortAudio
-        self.stream = self.p.open(format=self.sample_format, channels=self.channels,
-                                  rate=self.fs, frames_per_buffer=self.chunk, input=True)
-        self.isrecording = True
+        print(self.frames.__len__())
+        if self.isrecording == False and self.frames.__len__() == 0:
+            self.p = pyaudio.PyAudio()  # Create an interface to PortAudio
+            self.stream = self.p.open(format=self.sample_format, channels=self.channels,
+                                      rate=self.fs, frames_per_buffer=self.chunk, input=True)
+            self.isrecording = True
 
-        print('Recording')
-        t = threading.Thread(target=self.record)
-        t.start()
+            print('Recording')
+            t = threading.Thread(target=self.record)
+            t.start()
 
     # Main recording loop
     def record(self):
         while self.isrecording:
             data = self.stream.read(self.chunk)
+            intData = np.frombuffer(
+                self.stream.read(self.chunk), dtype=np.int16)
+
+            intframes.append(intData)
             self.frames.append(data)
 
     # Stop Recording and close the stream
@@ -149,10 +150,14 @@ class App:
             self.p.terminate()  # Terminate the PortAudio interface
 
             print('Finished recording')
-            print('filename'+str(self.count)+'.wav')
 
-            # Save the recorded data as a WAV file
-            wf = wave.open('filename'+str(self.count)+'.wav', 'wb')
+    # This executes on save button press and saves the file with the name chosen through radio buttons
+    # Save the recorded data as a WAV file
+    def save(self):
+        if self.frames.__len__() != 0 and self.fileName != '' and self.isrecording == False:
+            print(self.fileName)
+            print(self.frames.__len__())
+            wf = wave.open(self.fileName, 'wb')
             self.count = self.count + 1
             wf.setnchannels(self.channels)
             wf.setsampwidth(self.p.get_sample_size(self.sample_format))
@@ -160,9 +165,10 @@ class App:
             wf.writeframes(b''.join(self.frames))
             wf.close()
             self.frames.clear()
+            self.fileName = ''
 
 
 root = Tk()
 gui = App(root)
-ani = animation.FuncAnimation(f, animate, interval=1000)
+ani = animation.FuncAnimation(f, animate, interval=100)
 root.mainloop()
