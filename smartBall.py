e42551d33f478f57a6402752f06549cd8963eb7b
talhakhan1297 <talhakhan1297@yahoo.com>
2020-02-26 12:43:38 +0500

    Fixed Plot, Added Reset button and Made single Record/Stop button

--- smartBall.py
+++ smartBall.py
@@ -31,7 +31,26 @@ class App:
 
         # Initialized dynamic variables
         self.radioVal = StringVar()
-        self.count = 0
+        self.feedbackVal = StringVar()
+        self.rcdStpVal = StringVar()
+        self.rcdStpVal.set('Record')  # Initial value
+        self.radioVal.set('edge')  # Initial value
+        self.feedbackVal.set('')  # Initial value
+
+        try:
+            sampleCountFileR = open('sampleCount.txt', "r")
+            if sampleCountFileR.read() == '':
+                self.count = 0
+            else:
+                sampleCountFileR = open('sampleCount.txt', "r")
+                self.count = int(sampleCountFileR.read())
+            sampleCountFileR.close()
+
+        except FileNotFoundError:
+            sampleCountFileNW = open('sampleCount.txt', "w")
+            sampleCountFileNW.close()
+            self.count = 0
+
         self.frames = []  # Initialize array to store frames
         self.intframes = []
         self.isrecording = False
@@ -46,26 +65,29 @@ class App:
         master.resizable(False, False)
         master.config(bg=backgroundColor)
 
-        self.fig = Figure(figsize=(9, 3), dpi=100)
+        self.fig = Figure(figsize=(16, 5), dpi=60)
         self.a = self.fig.add_subplot(111)
+        self.a.set_title('Graph of Recorded Sound')
+        self.a.set_xlabel('No. of Samples')
+        self.a.set_ylabel('Amplitude')
 
         # Initialized Plot
         self.plotCanvas = FigureCanvasTkAgg(self.fig, master)
         self.plotCanvas.draw()
         self.plotCanvas.get_tk_widget().grid(
-            row=0, padx=(45, 45), pady=(24, 24), columnspan=5)
+            row=0, padx=(35, 35), pady=(24, 24), columnspan=5)
 
-        # Initialized Record Button
-        recordBtn = Button(master, text='Record', font=(
+        # Initialized Record/Stop Button
+        recordBtn = Button(master, textvariable=self.rcdStpVal, font=(
             "Montserrat 10 bold"), bg=accentColor, activebackground=pressedButton, fg=primaryColor, bd=0, pady=5, padx=5,
-            command=self.startRecording)
+            command=self.rcdStpSelect)
         recordBtn.grid(row=1, column=2, sticky='w')
 
         # Initialized Stop Button
-        stopBtn = Button(master, text='Stop', font=(
-            "Montserrat 10 bold"), bg=accentColor, activebackground=pressedButton, fg=primaryColor, bd=0, pady=5, padx=15,
-            command=self.stopRecording)
-        stopBtn.grid(row=1, column=2, sticky='e')
+        resetBtn = Button(master, text='Reset', font=(
+            "Montserrat 10 bold"), bg=accentColor, activebackground=pressedButton, fg=primaryColor, bd=0, pady=5, padx=10,
+            command=self.onReset)
+        resetBtn.grid(row=1, column=2, sticky='e')
 
         # Initialized Label
         note = Label(text="Please select a label for video file.", font=("Montserrat 13"),
@@ -108,66 +130,85 @@ class App:
             command=self.save)
         saveBtn.grid(row=4, column=2, pady=(24, 5), padx=15)
 
-    # Handle radio selection
+        # Initialized Feedback label
+        feedback = Label(textvariable=self.feedbackVal, font=("Montserrat 10"),
+                         fg=primaryColor, bg=backgroundColor)
+        feedback.grid(row=4, column=4, sticky='s', pady=10)
 
-    def radioSelect(self):
-        if self.frames.__len__() != 0 and self.isrecording == False:
-            self.fileName = str(self.count) + '_' + \
-                self.radioVal.get() + '.wav'
+    # Handle Record/Stop Button
+    def rcdStpSelect(self):
+        if self.isrecording == False:
+            self.startRecording()
+        else:
+            self.stopRecording()
+
+    # Handle Reset Button
+    def onReset(self):
+        if self.isrecording == False:
+            self.frames = []
+            self.a.clear()
 
     # Start Recording
     def startRecording(self):
-        print(self.frames.__len__())
         if self.isrecording == False and self.frames.__len__() == 0:
+            self.rcdStpVal.set('Stop')
             self.p = pyaudio.PyAudio()  # Create an interface to PortAudio
             self.stream = self.p.open(format=self.sample_format, channels=self.channels,
                                       rate=self.fs, frames_per_buffer=self.chunk, input=True)
             self.isrecording = True
 
-            print('Recording')
             t = threading.Thread(target=self.record)
             t.start()
+            self.feedbackVal.set('Recording ✓')
 
     # Main recording loop
     def record(self):
         while self.isrecording:
             data = self.stream.read(self.chunk)
-            intData = np.frombuffer(
-                self.stream.read(self.chunk), dtype=np.int16)
-
-            self.intframes.append(intData)
             self.frames.append(data)
+        self.frames = b''.join(self.frames)
 
     # Stop Recording and close the stream
     def stopRecording(self):
         if self.isrecording:
+            self.rcdStpVal.set('Record')
             self.isrecording = False
             self.stream.stop_stream()
             self.stream.close()
             self.p.terminate()  # Terminate the PortAudio interface
 
-            # self.fig = Figure(figsize=(9, 3), dpi=100)
-            # self.a = self.fig.add_subplot(111)
-            self.a.plot(self.intframes)
+            amplitude = np.frombuffer(self.frames, np.int16)
+            self.a.plot(amplitude)
             self.plotCanvas.draw()
+            self.feedbackVal.set('Stopped ✓')
 
-            print('Finished recording')
+    # Handle radio selection
+    def radioSelect(self):
+        if self.frames.__len__() != 0 and self.isrecording == False:
+            self.fileName = str(self.count) + '_' + \
+                self.radioVal.get() + '.wav'
+            self.feedbackVal.set('Selected ✓')
 
     # This executes on save button press and saves the file with the name chosen through radio buttons
     # Save the recorded data as a WAV file
     def save(self):
         if self.frames.__len__() != 0 and self.fileName != '' and self.isrecording == False:
-            print(self.fileName)
-            print(self.frames.__len__())
             wf = wave.open(self.fileName, 'wb')
-            self.count = self.count + 1
             wf.setnchannels(self.channels)
             wf.setsampwidth(self.p.get_sample_size(self.sample_format))
             wf.setframerate(self.fs)
-            wf.writeframes(b''.join(self.frames))
+            wf.writeframes(self.frames)
             wf.close()
-            self.frames.clear()
+
+            self.count = self.count + 1
+            sampleCountFileW = open('sampleCount.txt', "w")
+            sampleCountFileW.write(str(self.count))
+            sampleCountFileW.close()
+
+            self.frames = []
             self.fileName = ''
+            self.a.clear()
+            self.feedbackVal.set('Saved ✓')
 
 
 root = Tk()
